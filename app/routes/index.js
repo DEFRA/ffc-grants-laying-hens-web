212edf4a14575d306ab5ce368542def583289495
Yunus FIRAT <yunusfirat44@gmail.com>
2024-01-16 16:20:18 +0000

    update.

--- app/routes/index.js
+++ app/routes/index.js
@@ -24,5 +24,4 @@ const drawSectionPostRequests = (section) => {
 
 let pages = questionBank.sections.map(section => drawSectionGetRequests(section))
 pages = [...pages, ...questionBank.sections.map(section => drawSectionPostRequests(section))]
-pages.push(require('./project-summary'))
 module.exports = pages
