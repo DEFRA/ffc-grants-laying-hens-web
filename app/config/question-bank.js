16278bf77053a8aaee8a9e4bb434811060061f75
Oksana Kostenko <oksana.kostenko@capgemini.com>
2024-01-29 18:38:51 +0000

    ventilation-air-quality

--- app/config/question-bank.js
+++ app/config/question-bank.js
@@ -729,8 +729,7 @@ const questionBank = {
                     <li>water-tight roof and walls</li>
                     <li>capped roof and wall inlets and outlets</li>
                     <li>catch trays under all chimneys and roof-mounted vents.</li>
-                  </ul>
-            </span>`
+                  </ul>`
           },
           pageTitle: '',
           url: 'building-items',
@@ -1386,6 +1385,67 @@ const questionBank = {
             }
           ],
           yarKey: 'henVentilationRate'
+        },
+        {
+          key: 'ventilation-air-quality',
+          order: 104,
+          title: 'Will the ventilation maintain the required air quality parameters?',
+          pageTitle: '',
+          url: 'ventilation-air-quality',
+          baseUrl: 'ventilation-air-quality',
+          backUrlObject: {
+            dependentQuestionYarKey: 'poultryType',
+            dependentAnswerKeysArray: ['poultry-type-A1'],
+            urlOptions: {
+              thenUrl: 'hen-ventilation-rate',
+              elseUrl: 'pullet-ventilation-rate'
+            }
+          },
+          nextUrl: 'lighting-system',
+          ineligibleContent: {
+            messageContent: `
+            <div class="govuk-list govuk-list--bullet">
+            <p class="govuk-body">The ventilation must maintain air quality (at bird head height of less than 50cm) at a:</p>
+                  <ul>
+                    <li>carbon dioxide level of less than 3,000 parts per million (ppm)</li>
+                    <li>percentage relative humidity (%rH) level of 40% to 70%</li>
+                  </ul>
+            </div>`,
+            messageLink: {
+              url: 'https://www.gov.uk/government/organisations/rural-payments-agency',
+              title: 'See other grants you may be eligible for.'
+            }
+          },
+        hint: {
+          html: `
+                <p>The ventilation must maintain air quality (at bird head height of less than 50cm) at a:</p>
+                <ul class="govuk-list--bullet">
+                    <li>carbon dioxide level of less than 3,000 parts per million (ppm)</li>
+                    <li>percentage relative humidity (%rH) level of 40% to 70%</li>
+                </ul>`
+        },
+          fundingPriorities: '',
+          type: 'single-answer',
+          minAnswerCount: 1,
+          classes: 'govuk-radios--inline govuk-fieldset__legend--l',
+          validate: [
+            {
+              type: 'NOT_EMPTY',
+              error: 'Select yes if the lighting system will provide non-flicker light from dimmable LEDs'
+            }
+          ],
+          answers: [
+            {
+              key: 'ventilation-air-quality-A1',
+              value: 'Yes'
+            },
+            {
+              key: 'ventilation-air-quality-A2',
+              value: 'No',
+              notEligible: true
+            }
+          ],
+          yarKey: 'ventilationAirQuality'
         },
         {
           key: 'project-cost',
---
+++ test/integration/narrow/routes/ventilation-air-quality.test.js
@@ -0,0 +1,92 @@
+const { crumbToken } = require('./test-helper')
+
+describe('Page: /ventilation-air-quality', () => {
+  let varList = {
+    poultryType: 'Laying Hens'
+  }
+  
+  jest.mock('../../../../app/helpers/session', () => ({
+    setYarValue: (request, key, value) => null,
+    getYarValue: (request, key) => {
+      if (varList[key]) return varList[key]
+      else return undefined
+    }
+  }))
+
+  it('page loads successfully, with all the options', async () => {
+    const options = {
+      method: 'GET',
+      url: `${global.__URLPREFIX__}/ventilation-air-quality`
+    }
+
+    const response = await global.__SERVER__.inject(options)
+    expect(response.statusCode).toBe(200)
+    expect(response.payload).toContain('Will the ventilation maintain the required air quality parameters?')
+    expect(response.payload).toContain('Yes')
+    expect(response.payload).toContain('No')
+  })
+
+  it('no option selected -> show error message', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/ventilation-air-quality`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { crumb: crumbToken }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(200)
+    expect(postResponse.payload).toContain('Select yes if the lighting system will provide non-flicker light from dimmable LEDs')
+  })
+
+  it('user selects eligible option -> store user response and redirect to /lighting-system', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/ventilation-air-quality`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { ventilationAirQuality: 'Yes',  crumb: crumbToken }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(302)
+    expect(postResponse.headers.location).toBe('lighting-system')
+  })
+
+  it('user selects ineligible option `No` -> display ineligible page', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/ventilation-air-quality`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { ventilationAirQuality: 'No', crumb: crumbToken }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.payload).toContain('You cannot apply for a grant from this scheme')
+    expect(postResponse.payload).toContain('The ventilation must maintain air quality (at bird head height of less than 50cm) at a:')
+    expect(postResponse.payload).toContain('carbon dioxide level of less than 3,000 parts per million (ppm)')
+    expect(postResponse.payload).toContain('percentage relative humidity (%rH) level of 40% to 70%')
+    expect(postResponse.payload).toContain('See other grants you may be eligible for.')
+  })
+
+  it('page loads with correct back link - /hen-ventilation-rate - if poultry type is Laying Hens', async () => {
+    varList.poultryType = 'hen'
+    const options = {
+      method: 'GET',
+      url: `${global.__URLPREFIX__}/ventilation-air-quality`
+    }
+    const response = await global.__SERVER__.inject(options)
+    expect(response.statusCode).toBe(200)
+    expect(response.payload).toContain('<a href=\"hen-ventilation-rate\" class=\"govuk-back-link\">Back</a>')
+  })
+
+  it('page loads with correct back link - /pullet-ventilation-rate - if poultry type is Pullets', async () => {
+    varList.poultryType = 'Pullets'
+    const options = {
+      method: 'GET',
+      url: `${global.__URLPREFIX__}/ventilation-air-quality`
+    }
+    const response = await global.__SERVER__.inject(options)
+    expect(response.statusCode).toBe(200)
+    expect(response.payload).toContain('<a href=\"pullet-ventilation-rate\" class=\"govuk-back-link\">Back</a>')
+  })
+})
