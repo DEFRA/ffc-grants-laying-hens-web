6f2ff2c0104bfd97bb9c6317ed14e77ae373c687
Yunus FIRAT <yunusfirat44@gmail.com>
2024-02-22 00:49:07 +0000

    coverage up ?

--- test/integration/narrow/routes/1000-birds.test.js
+++ test/integration/narrow/routes/1000-birds.test.js
@@ -38,20 +38,6 @@ describe('Page: /1000-birds', () => {
     expect(postResponse.statusCode).toBe(200)
     expect(postResponse.payload).toContain('Select yes if you currently have at least 1,000 laying hens on your farm')
   })
-
-  it('user selects eligible option -> store user response and redirect to /building-items', async () => {
-    const postOptions = {
-      method: 'POST',
-      url: `${global.__URLPREFIX__}/1000-birds`,
-      headers: { cookie: 'crumb=' + crumbToken },
-      payload: { birdNumber: 'Yes',  crumb: crumbToken }
-    }
-
-    const postResponse = await global.__SERVER__.inject(postOptions)
-    expect(postResponse.statusCode).toBe(302)
-    expect(postResponse.headers.location).toBe('building-items')
-  })
-
   it('user selects ineligible option `No` -> display ineligible page', async () => {
     const postOptions = {
       method: 'POST',
@@ -64,6 +50,58 @@ describe('Page: /1000-birds', () => {
     expect(postResponse.payload).toContain('You cannot apply for a grant from this scheme')
     expect(postResponse.payload).toContain('You must keep at least 1,000 laying hens on your farm currently.')
     expect(postResponse.payload).toContain('See other grants you may be eligible for.')
+  })
+  
+  it('page loads successfully, with all the options', async () => {
+    varList.poultryType = 'pullets'
+    const options = {
+      method: 'GET',
+      url: `${global.__URLPREFIX__}/1000-birds`
+    }
+
+    const response = await global.__SERVER__.inject(options)
+    expect(response.statusCode).toBe(200)
+    expect(response.payload).toContain('Do you keep at least 1,000 pullets on your farm currently?')
+    expect(response.payload).toContain('Yes')
+    expect(response.payload).toContain('No')
+  })
+
+  it('no option selected -> show error message', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/1000-birds`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { crumb: crumbToken }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(200)
+    expect(postResponse.payload).toContain('Select yes if you currently have at least 1,000 pullets on your farm')
+  })
+  it('user selects ineligible option `No` -> display ineligible page', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/1000-birds`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { birdNumber: 'No', crumb: crumbToken }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.payload).toContain('You cannot apply for a grant from this scheme')
+    expect(postResponse.payload).toContain('You must keep at least 1,000 pullets on your farm currently.')
+    expect(postResponse.payload).toContain('See other grants you may be eligible for.')
+  })
+  it('user selects eligible option -> store user response and redirect to /building-items', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/1000-birds`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { birdNumber: 'Yes',  crumb: crumbToken }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(302)
+    expect(postResponse.headers.location).toBe('building-items')
   })
 
   it('page loads with correct back link - /poultry-type', async () => {
