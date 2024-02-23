e61d1e73021b2c5aee5292c0bd9b62d45f4b3ceb
Yunus FIRAT <yunusfirat44@gmail.com>
2024-02-23 11:24:03 +0000

    remove console.log

--- app/helpers/urls.js
+++ app/helpers/urls.js
@@ -13,7 +13,6 @@ const getUrl = (urlObject, url, request, secBtn, _currentUrl) => {
 
   const { dependentQuestionYarKey, dependentAnswerKeysArray, urlOptions, dependentElseUrlYarKey,dependentElseUrlQuestionKey, dependentElseUrlAnswerKey, nonDependentAnswerKeysArray = [] } = urlObject
   let { thenUrl, elseUrl, nonDependentUrl, dependantElseUrl } = urlOptions
-  console.log('dependentQuestionYarKey', dependentElseUrlQuestionKey)
   const dependentAnswer = getYarValue(request, dependentQuestionYarKey)
 
   if (dependentQuestionYarKey === 'SolarPVCost') {
