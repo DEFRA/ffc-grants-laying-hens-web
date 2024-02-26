0c823308346590ff72f3cd855e65e72fdc4eaea2
Oksana Kostenko <oksana.kostenko@capgemini.com>
2024-02-26 17:19:50 +0000

    validation upd

--- app/assets/src/scripts/validation.js
+++ app/assets/src/scripts/validation.js
@@ -2,22 +2,24 @@ export function validation () {
   let elements = document.querySelectorAll('input[type="checkbox"]')
   const nonEligibleOptions = ['None of the above', 'I will not monitor any poultry management data']
 
-   const elementsArr = Array.from(elements)
-   elementsArr.filter(el => nonEligibleOptions.includes(el.value)).forEach((element) => {
-    element.addEventListener('change', (event) => {
-      event.preventDefault()
-
-      const remainingElements = elementsArr.filter(el => !nonEligibleOptions.includes(el.value))
-      remainingElements.forEach((checkBox) => {
-        checkBox.disabled = element.checked
-        checkBox.checked = false
+    if (elements) {
+      const elementsArr = Array.from(elements)
+      elementsArr.filter(el => nonEligibleOptions.includes(el.value)).forEach((element) => {
+        element.addEventListener('change', (event) => {
+          event.preventDefault()
+    
+          const remainingElements = elementsArr.filter(el => !nonEligibleOptions.includes(el.value))
+          remainingElements.forEach((checkBox) => {
+            checkBox.disabled = element.checked
+            checkBox.checked = false
+          })
+        })
+      
+        // in case you back to this page
+        if (element.checked) {
+          const event = new Event('change')
+          element.dispatchEvent(event)
+        }
       })
-    })
-  
-    // in case you back to this page
-    if (element.checked) {
-      const event = new Event('change')
-      element.dispatchEvent(event)
     }
-  })
-}
+  }
