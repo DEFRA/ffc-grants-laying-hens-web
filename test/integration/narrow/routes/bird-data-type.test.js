4603fe2d30f8847cf4c3fd5ab9663dd6dbe0bf8b
Oksana Kostenko <oksana.kostenko@capgemini.com>
2024-02-26 16:46:49 +0000

    test fix

--- test/integration/narrow/routes/bird-data-type.test.js
+++ test/integration/narrow/routes/bird-data-type.test.js
@@ -1,15 +1,10 @@
+const { commonFunctionsMock } = require('../../../session-mock')
 const { crumbToken, testNextLink } = require('./test-helper')
 
 describe('Page: /bird-data-type', () => {
     let varList = {}
 
-    jest.mock('../../../../app/helpers/session', () => ({
-        setYarValue: (request, key, value) => null,
-        getYarValue: (request, key) => {
-          if (varList[key]) return varList[key]
-          else return undefined
-        }
-    }))
+  commonFunctionsMock(varList, undefined)
 
   it('page loads successfully, with all the options', async () => {
     const options = {
