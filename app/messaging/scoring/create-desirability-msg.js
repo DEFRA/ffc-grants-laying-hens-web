303781bd4f8ad683739c87d82c9aa4ea5608ba1c
diya2214 <madihakhn@yahoo.com>
2024-03-25 10:50:19 +0000

    some loga

--- app/messaging/scoring/create-desirability-msg.js
+++ app/messaging/scoring/create-desirability-msg.js
@@ -3,8 +3,8 @@ const { getQuestionAnswer } = require('ffc-grants-common-functionality').utils
 const { ALL_QUESTIONS } = require('../../config/question-bank')
 
 const { desirabilityInputQuestionMapping, desirabilityQuestions: questionContent } = require('./content-mapping')
-const desirabilityQuestionsHen = ['poultry-type', 'current-system', 'current-multi-tier-system', 'ramp-connection', 'maximum-tier-height', 'three-tiers', 'hen-multi-tier', 'natural-light', 'easy-grip-perches', 'building-biosecurity', 'pollution-mitigation', 'renewable-energy', 'bird-data-type', 'environmental-data-type']
-const desirbailityQuestionsPullet = ['poultry-type', 'current-system', 'current-multi-tier-system', 'ramp-connection', 'maximum-tier-height', 'three-tiers', 'pullet-multi-tier', 'natural-light', 'dark-brooders', 'easy-grip-perches', 'building-biosecurity', 'pollution-mitigation', 'pullet-veranda-features', 'renewable-energy', 'bird-data-type', 'environmental-data-type']
+const desirabilityQuestionsHen = ['current-system', 'current-multi-tier-system', 'ramp-connection', 'maximum-tier-height', 'three-tiers', 'hen-multi-tier', 'natural-light', 'easy-grip-perches', 'building-biosecurity', 'pollution-mitigation', 'renewable-energy', 'bird-data-type', 'environmental-data-type']
+const desirbailityQuestionsPullet = ['current-system', 'current-multi-tier-system', 'ramp-connection', 'maximum-tier-height', 'three-tiers', 'pullet-multi-tier', 'natural-light', 'dark-brooders', 'easy-grip-perches', 'building-biosecurity', 'pollution-mitigation', 'pullet-veranda-features', 'renewable-energy', 'bird-data-type', 'environmental-data-type']
 
 const POULTRY_TYPE_HENS = getQuestionAnswer('poultry-type', 'poultry-type-A1', ALL_QUESTIONS)
 
@@ -20,7 +20,7 @@ function getUserAnswer (answers, userInput) {
 function getDesirabilityDetails(questionKey, userInput) {
   console.log('HELLLLLOOOO', questionKey,'-----',userInput )
   const content = questionContent[questionKey]
-
+console.log(content,'CCCCCCCCCCCCCCCCCC')
   return {
     key: questionKey,
     answers: content.map(({ key, title, answers }) => ({
@@ -39,6 +39,7 @@ function getDesirabilityDetails(questionKey, userInput) {
 function desirability (userInput) {
   const isHens = userInput.poultryType === POULTRY_TYPE_HENS
   const validKeys = isHens ? desirabilityQuestionsHen : desirbailityQuestionsPullet
+  console.log(validKeys,'kkkkkkkkeeeeyyyysssss')
 
   return {
     grantScheme: {
