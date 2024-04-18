03c62fa36e66db1977b4832ab51a3c6d3e451752
Yunus FIRAT <yunusfirat44@gmail.com>
2024-04-18 18:09:19 +0100

    test fix.

--- test/integration/narrow/routes/project-type.test.js
+++ test/integration/narrow/routes/project-type.test.js
@@ -79,7 +79,6 @@ it('user selects ineligible option: \'None of the above\' -> display ineligible 
 
   it('user selects `Adding a veranda only to the existing building` -> store user response and redirect to /veranda-funding-cap', async () => {
     process.env.VERANDA_FUNDING_CAP = 'true'
-
     const postOptions = {
       method: 'POST',
       url: `${global.__URLPREFIX__}/project-type`,
@@ -90,19 +89,6 @@ it('user selects ineligible option: \'None of the above\' -> display ineligible 
     const postResponse = await global.__SERVER__.inject(postOptions)
     expect(postResponse.statusCode).toBe(302)
     expect(postResponse.headers.location).toBe('applicant-type')
-  })
-
-  it('user selects `Adding a veranda only to the existing building` -> store user response and redirect to /veranda-funding-cap', async () => {
-    const postOptions = {
-      method: 'POST',
-      url: `${global.__URLPREFIX__}/project-type`,
-      headers: { cookie: 'crumb=' + crumbToken },
-      payload: { projectType: 'Adding a veranda only to the existing building', crumb: crumbToken }
-    }
-
-    const postResponse = await global.__SERVER__.inject(postOptions)
-    expect(postResponse.statusCode).toBe(302)
-    expect(postResponse.headers.location).toBe('/laying-hens/veranda-funding-cap')
   })
 
   it('page loads with correct back link ', async () => {
