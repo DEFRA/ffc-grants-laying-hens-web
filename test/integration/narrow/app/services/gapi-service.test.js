9f91db2fd02bfda3e9a19de8fabcb9da3439b6c1
zavies97 <zavies97@users.noreply.github.com>
2024-05-09 14:55:34 +0100

    update mopck solely for test coverage in PR

--- test/integration/narrow/app/services/gapi-service.test.js
+++ test/integration/narrow/app/services/gapi-service.test.js
@@ -36,7 +36,7 @@ describe('sendGAEvent', () => {
         info: { hostname: 'localhost' },
         ga: { view: gaViewMock },
       };
-      metrics = { name: 'eligibility_passed', params: {} };
+      metrics = { name: 'confirmation', params: {} };
     });
   
     it('should send the correct event', async () => {
@@ -46,6 +46,7 @@ describe('sendGAEvent', () => {
 
     it('should send the correct event - no score', async () => {
       varList['current-score'] = null
+
       await sendGAEvent(request, metrics);
       expect(gaViewMock).toHaveBeenCalledWith(request, [{ name: metrics.name, params: expect.any(Object) }]);
     });
